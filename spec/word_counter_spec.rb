8bf4ce5790efdf1fde57cf0aeae898b526c95733
Andrew Frank <afrank@contactusinc.com>
2015-04-21 11:12:42 -0400

    Adding initial spec to copy the example specified in the README.md file.

---
+++ spec/word_counter_spec.rb
@@ -0,0 +1,24 @@
+require 'word_counter'
+
+describe WordCounter do
+  describe '.count_words' do
+    When(:result) { subject.count_words(test_words) }
+
+    context 'example from README file' do
+      Given(:test_words) {
+        "The cat is tall. The cat is much bigger than the mouse."
+      }
+      Then {
+        result == {
+          'the'    => 3,
+          'cat'    => 2,
+          'is'     => 1,
+          'mouse'  => 1,
+          'bigger' => 1,
+          'than'   => 1,
+          'much'   => 1
+        }
+      }
+    end # example from README file
+  end # .count_words
+end
