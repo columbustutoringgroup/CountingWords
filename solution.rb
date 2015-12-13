5f7e92a7c8538dd2142b9f5745f8220943c232d2
Samuel Priddy <spriddy@covermymeds.com>
2015-12-13 17:10:32 -0500

    Move logic into functions with reasonable names

--- solution.rb
+++ solution.rb
@@ -1,10 +1,29 @@
-text = File.read('speech.txt')
-words = text.downcase.scan(/\w+[a-z']*\w+/)
+TEXT = File.read('speech.txt')
 
-words_count = Hash.new(0)
-words.each { |word| words_count[word] += 1}
-sorted_word_counts = words_count.to_a.sort_by { |word| -word[1] }
-
-sorted_word_counts.each do |word|
-  puts "#{word[0].capitalize}  --  #{word[1]}"
+def find_words text
+  words_regex = /\w+[a-z']*\w+/
+  text.downcase.scan words_regex
 end
+
+def count_words words_array
+  Hash.new(0).tap do |word_count|
+    words_array.each { |word| word_count[word] += 1 }
+  end
+end
+
+def sort_words words_frequency_hash
+  words_frequency_hash.sort_by { |word, count| -count }
+end
+
+def wordcount_print_format word
+  "#{word[1]} - #{word[0].capitalize}"
+end
+
+def print_sorted_word_counts sorted_words
+  sorted_words.each { |word| puts wordcount_print_format(word) }
+end
+
+words = find_words TEXT
+word_freqs = count_words words
+sorted_words = sort_words word_freqs
+print_sorted_word_counts sorted_words
