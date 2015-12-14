165e1f4e37072058cea7ccf0909a3859618a59a8
Samuel Priddy <spriddy@covermymeds.com>
2015-12-14 09:56:40 -0500

    Move methods into a class

--- solution.rb
+++ solution.rb
@@ -1,29 +1,39 @@
 TEXT = File.read('speech.txt')
 
-def find_words text
-  words_regex = /\w+[a-z']*\w+/
-  text.downcase.scan words_regex
-end
 
-def count_words words_array
-  Hash.new(0).tap do |word_count|
-    words_array.each { |word| word_count[word] += 1 }
+class WordCounter
+  @@WORDS_REGEX = /\w+[a-z']*\w+/
+
+  def self.solve text
+    words = find_words TEXT
+    word_freqs = count_words words
+    sorted_words = sort_words word_freqs
+    print_sorted_word_counts sorted_words
+  end
+
+  def self.find_words text
+    text.downcase.scan @@WORDS_REGEX
+  end
+
+  def self.count_words words_array
+    Hash.new(0).tap do |word_count|
+      words_array.each { |word| word_count[word] += 1 }
+    end
+  end
+
+  def self.sort_words words_frequency_hash
+    words_frequency_hash.sort_by { |word, count| -count }
+  end
+
+  def self.wordcount_print_format word
+    "#{word[1]} - #{word[0].capitalize}"
+  end
+
+  def self.print_sorted_word_counts sorted_words
+    sorted_words.each { |word| puts wordcount_print_format(word) }
   end
 end
 
-def sort_words words_frequency_hash
-  words_frequency_hash.sort_by { |word, count| -count }
-end
 
-def wordcount_print_format word
-  "#{word[1]} - #{word[0].capitalize}"
-end
+WordCounter.solve(TEXT)
 
-def print_sorted_word_counts sorted_words
-  sorted_words.each { |word| puts wordcount_print_format(word) }
-end
-
-words = find_words TEXT
-word_freqs = count_words words
-sorted_words = sort_words word_freqs
-print_sorted_word_counts sorted_words
