b5184f7f06c4f92b3759f88df7d1820afc97ec02
maxdignan <maxdignan@gmail.com>
2015-12-27 22:34:06 -0500

    made corrections based on feedback

--- parser.rb
+++ parser.rb
@@ -1,10 +1,10 @@
 
 class Parser
   def initialize(filename)
-    @file = File.open(filename, "r")
+    file = File.open(filename, "r")
     @text = ''
-    @file.each_line do |txt|
-      @text = @text + txt
+    file.each_line do |txt|
+      @text += txt
     end
   end
 
@@ -14,23 +14,21 @@ class Parser
 
   def map_words_to_count
     array = array_of_words
-    obj = {}
+    hash = {}
     array.each do |item|
       item = item.downcase
-      obj[item] = obj[item] + 1 if obj[item] != nil
-      obj[item] = 1 if obj[item] == nil
+      if hash[item] == nil
+        hash[item] = 1
+      else
+        hash[item] += 1
+      end
     end
-    obj
+    hash
   end
 
   def decreasing_order_word_count
-    map = map_words_to_count
-    out_array = []
-    map.each do |item|
-      new_obj = {name: item[0], value: item[1]}
-      out_array.push new_obj
-    end
-    out_array.sort_by {|item| item[:value]}.reverse
+    map_arr = map_words_to_count.to_a
+    map_arr = map_arr.sort_by {|tuple| tuple[1]}.reverse
   end
 end
 
@@ -39,5 +37,5 @@ speechParser = Parser.new './speech.txt'
 map_of_words_and_counts = speechParser.decreasing_order_word_count
 
 map_of_words_and_counts.each do |item|
-  puts "#{item[:name]}, #{item[:value]}"
+  puts "#{item[0]} => #{item[1]}"
 end
