4ff643020791072cabf954e85627630b54476aab
Austen Madden <maddenausten@gmail.com>
2016-01-13 15:54:27 -0500

    Slight tweaks

--- wordCount.rb
+++ wordCount.rb
@@ -1,25 +1,25 @@
 class WordCounter
   attr_accessor :file, :input, :output
   def initialize(file)
-    @file = file
-    @input = ""
-    @output =  Hash.new(0)
+    self.file = file
+    self.input = ""
+    self.output =  Hash.new(0)
   end
 
   def parse
-    File.foreach(@file) {|x| count(x)}
+    File.foreach(file) {|x| count(x)}
   end
 
   def print
-    @output = @output.sort_by {|key, value| value}
-    @output.each {|key, value| puts "#{value} - #{key}"}
+    self.output = output.sort_by {|key, value| value}
+    output.each {|key, value| puts "#{value} - #{key}"}
   end
 
   private
   def count(input)
     input = input.split
     input.each do |word|
-      @output[word] += 1
+      output[word] += 1
     end
   end
 end
