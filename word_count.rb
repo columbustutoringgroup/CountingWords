c75dd9117df23ca99eb019f4f3061d1991d9428f
Kyle Modlich <kmodlich@cmdvmosxlt191.innova.local>
2016-05-27 16:43:40 -0400

    while single words are useless for meaning, they are words

--- word_count.rb
+++ word_count.rb
@@ -1,5 +1,5 @@
 file = File.open("./speech.txt",'rb')
-WORD_REGEX = /[a-zA-Z]+['-]?[a-zA-Z]+/
+WORD_REGEX = /[a-zA-Z]+['-]?[a-zA-Z]*/
 words = file.read.scan(WORD_REGEX).map(&:downcase).group_by{|val|val}.sort_by{|k,v|-v.count}
 words.each{|pair|
 	puts "#{pair[0]}: #{pair[1].count}"
