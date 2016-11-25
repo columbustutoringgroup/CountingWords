8e5e064c39314065c72ba0fa7e45ba118ffa63d5
drm2 <davidrmyersii@gmail.com>
2016-11-25 13:06:58 -0500

    Cleaning up WordCounter. Allowing apostrophes in words (instead of removing them). Updating test arrays. Implementing count_from_file method instead of loading file in test directly.

--- word_counter.rb
+++ word_counter.rb
@@ -3,20 +3,25 @@ class WordCounter
   end
 
   def count(text)
-    # parsed = /[a-z]+/s.match(text.downcase)
-    parsed = text.downcase.gsub(/[^\w\s\n]/, '').gsub(/[\n\s]+/, ' ').strip
+    parsed = text.downcase.gsub(/[^\w\s\n']+/, '').gsub(/[\n\s]+/, ' ').strip
     words = parsed.split(' ')
 
     list = {}
 
-    words.each { |word|
+    words.each do |word|
       if list.key?(word)
         list[word] += 1
       else
         list[word] = 1
       end
-    }
+    end
 
     list.sort_by { |key, value| value * -1 }
+  end
+
+  def count_from_file(file)
+    text = File.open(file, 'r') { |f| f.read }
+
+    count(text)
   end
 end
